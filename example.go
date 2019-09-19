181c5b845cc5a566f7eca67b2c8b3423f7504e2f
AIllIll <864147974@qq.com>
2019-09-19 18:48:44 +0800

    v0.3

--- example.go
+++ example.go
@@ -1,32 +1,49 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"github.com/kataras/iris"
-	"go.mongodb.org/mongo-driver/bson"
 	"io/ioutil"
-	"log"
 	"net/http"
-	"reflect"
 	"strings"
 	"time"
 	"github.com/iris-contrib/middleware/cors"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
+
+type ServerLog struct {
+	time string
+	test string
+}
 
 type Machine struct{
 	name string
 	value float64
 }
 
+var session *mgo.Session
+var database *mgo.Database
+var collection_server_log *mgo.Collection
+var collection_building *mgo.Collection
+var collection_machine *mgo.Collection
 
 func main() {
-	db_ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	client, err := mongo.Connect(db_ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-	fmt.Println(err)
-	collection := client.Database("laundryMachineSJTU").Collection("machines")
+	// init mongo client
+	session, err := mgo.Dial("localhost:27017")
+	if err != nil {
+		panic(nil)
+	}
+	defer session.Clone()
+	session.SetMode(mgo.Monotonic, true)
+	database = session.DB("sjtu")
+	collection_building := database.C("building")
+	collection_machine := database.C("machine")
+	collection_server_log := database.C("server_log")
+	collection_server_log.Insert(bson.M{"time": time.Now(), "test": '0'})
+	fmt.Println(bson.M{"time": time.Now()})
+
+	// init iris
 	app := iris.New()
 	crs := cors.New(cors.Options{
 		AllowedOrigins:   []string{"*", "http://localhost:8080", "http://192.168.1.101:8080", "http://111.186.2.209:19034"},   //允许通过的主机名称
@@ -35,60 +52,71 @@ func main() {
 	v1 := app.Party("/", crs).AllowMethods(iris.MethodOptions) // <- 对于预检很重要。
 	{
 		v1.Get("/ping", func(ctx iris.Context) {
-			db_ctx, _ = context.WithTimeout(context.Background(), 5*time.Second)
-			res, err := collection.InsertOne(db_ctx, bson.M{"name": "pi", "value": 3.14159})
-			fmt.Println(err)
-			id := res.InsertedID
 			ctx.JSON(iris.Map{
-				"message": id,
+				"serverTime": time.Now(),
 			})
-		})
-		v1.Get("/test", func(ctx iris.Context) {
-			id := ctx.URLParam("id")
-			fmt.Println(reflect.TypeOf(id))
-			c := http.Client{Timeout: 5 * time.Second}
-			res, _ := c.Get("https://www.weimaqi.net/Index_.aspx?device_name=" + id)
-			defer res.Body.Close()
-			body, _ := ioutil.ReadAll(res.Body)
-			fmt.Println(string(body))
-			if strings.Contains(string(body), "离线") {
-				ctx.HTML("<h1> 离线 </h1>")
-			} else {
-				ctx.HTML("<h1> 可以用 </h1>")
-			}
 		})
 		v1.Get("/check", func(ctx iris.Context) {
 			id := ctx.URLParam("id")
-			fmt.Println(reflect.TypeOf(id))
 			c := http.Client{Timeout: 5 * time.Second}
 			res, _ := c.Get("https://www.weimaqi.net/Index_.aspx?device_name=" + id)
 			defer res.Body.Close()
 			body, _ := ioutil.ReadAll(res.Body)
-			fmt.Println(string(body))
+			// fmt.Println(string(body))
 			if strings.Contains(string(body), "离线") {
 				ctx.JSON(iris.Map{"status": "offline"},)
 			} else {
 				ctx.JSON(iris.Map{"status": "online"},)
 			}
 		})
-		v1.Get("/getList", func(ctx iris.Context) {
-			building := ctx.URLParam("building") // the name of the building
-			fmt.Println(reflect.TypeOf(building))
-
-			db_ctx, _ = context.WithTimeout(context.Background(), 30*time.Second)
-			cur, err := collection.Find(db_ctx, bson.D{})
-			if err != nil { log.Fatal(err) }
-			defer cur.Close(db_ctx)
-			for cur.Next(db_ctx) {
-				var result bson.M
-				err := cur.Decode(&result)
-				if err != nil { log.Fatal(err) }
-				fmt.Println(result["name"])
-			}
-			if err := cur.Err(); err != nil {
-				log.Fatal(err)
+		v1.Post("/addBuilding", func(ctx iris.Context) {
+			var formData iris.Map
+			ctx.ReadJSON(&formData)
+			name := formData["name"]
+			pwd := formData["pwd"]
+			fmt.Println(formData["name"])
+			fmt.Println(name, pwd)
+			if pwd == "meiyoumima" {
+				collection_building.Upsert(bson.M{"name": name}, bson.M{"name": name})
+				ctx.JSON(iris.Map{"Msg": "success"})
+			} else {
+				ctx.JSON(iris.Map{"Msg": "invalid password"})
 			}
 		})
+		v1.Get("/getBuildingList", func(ctx iris.Context) {
+			query := collection_building.Find(nil)
+			var result []iris.Map
+			query.All(&result)
+			ctx.JSON(result)
+		})
+		v1.Post("/addMachine", func(ctx iris.Context) {
+			var formData iris.Map
+			ctx.ReadJSON(&formData)
+			building := formData["building"]
+			name := formData["name"]
+			code := formData["code"]
+			pwd := formData["pwd"]
+			fmt.Println(building, name, code, pwd)
+			if pwd == "meiyoumima" {
+				num, err := collection_machine.Upsert(bson.M{"code": code}, bson.M{"building": building, "name": name, "code": code})
+				fmt.Println(num, err)
+				ctx.JSON(iris.Map{"Msg": "success"})
+			} else {
+				ctx.JSON(iris.Map{"Msg": "invalid password"})
+			}
+		})
+		v1.Get("/getMachineList", func(ctx iris.Context) {
+			building := ctx.URLParam("building")
+			if len(building)!= 0 {
+				query := collection_machine.Find(bson.M{"building": building})
+				var result []iris.Map
+				query.All(&result)
+				ctx.JSON(result)
+			} else {
+				ctx.JSON(iris.Map{"Msg": "invalid building"})
+			}
+		})
+
 	}
 	app.Run(iris.Addr(":8082"))
 }
